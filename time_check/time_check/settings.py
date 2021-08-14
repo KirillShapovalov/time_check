e4d177f3555484883246cd276d55015745404836
KirillShapovalov <kirill.shapovlov@gmail.com>
2021-08-14 17:55:53 +0300

    another variant for .env added (dotenv)

--- time_check/time_check/settings.py
+++ time_check/time_check/settings.py
@@ -11,11 +11,14 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from decouple import config
+import os
+from dotenv import load_dotenv, find_dotenv
 
-# time_api_url = config('time_api_url')
-# url = config('url')
-# delta = config('delta')
+load_dotenv(find_dotenv())
+
+DEBUG = os.environ.get('DEBUG')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -23,10 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', cast=bool, default=False)
 
 ALLOWED_HOSTS = []
 
